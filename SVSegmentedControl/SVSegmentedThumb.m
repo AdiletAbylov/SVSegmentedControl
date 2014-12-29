24c027b3bf398ea0f95d2a763e0d97b7e4b2839f
Adilet Abylov <a.abylov@gmail.com>
2014-12-29 15:51:30 +0600

    Set background image size with bottom inset

--- SVSegmentedControl/SVSegmentedThumb.m
+++ SVSegmentedControl/SVSegmentedThumb.m
@@ -129,7 +129,7 @@
         _thumbBackgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(5+self.segmentedControl.thumbEdgeInset.left,
                                                                                   self.segmentedControl.thumbEdgeInset.top,
                                                                                   self.bounds.size.width-10-self.segmentedControl.thumbEdgeInset.left-self.segmentedControl.thumbEdgeInset.right,
-                                                                                  self.bounds.size.height)];
+                                                                                  self.bounds.size.height - self.segmentedControl.thumbEdgeInset.bottom)];
         _thumbBackgroundImageView.backgroundColor = [UIColor clearColor];
         [self insertSubview:_thumbBackgroundImageView atIndex:0];
         
